12c36b2ffeaca9e78711fcb63ffabf02d8a5a8ee
stefanprobst <stefan.probst@univie.ac.at>
2020-09-08 20:30:00 +0200

    feat: add eslint config

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-next-component
+
+Shareable ESLint config for Next.js component packages. See the [ESLint docs](https://eslint.org/docs/user-guide/configuring) for installation instructions.
---
+++ eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
