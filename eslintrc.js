617d12e195ad9bf2be2d1b914722ab4ea0d59233
Stefan Probst <stefanprobst@kolabnow.com>
2020-09-09 12:33:52 +0200

    feat: add import and jest plugins

--- eslintrc.js
+++ eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
   root: true,
   parserOptions: {
+    project: ["./tsconfig.json"],
     sourceType: "module",
   },
   env: {
     browser: true,
-    es2020: true,
+    "jest/globals": true,
     node: true,
+    es2020: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
@@ -19,11 +27,31 @@ module.exports = {
     "prettier/@typescript-eslint",
   ],
   settings: {
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+    },
     react: {
       version: "detect",
     },
   },
   rules: {
+    "no-var": ["error"],
+    "prefer-const": ["error"],
+    "import/order": [
+      "error",
+      { alphabetize: { order: "asc", caseInsensitive: true } },
+    ],
+    "import/no-anonymous-default-export": "error",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "jsx-a11y/no-onchange": "off",
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
   },
